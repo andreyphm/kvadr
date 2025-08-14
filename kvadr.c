c62c7088e62f60e3daf52810fd7f7bf72f6d4bb0
andreyphm <andrewkrokhin@gmail.com>
2025-08-14 15:09:20 +0300

    проблемки 11 строка

--- kvadr.c
+++ kvadr.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
-int main() {
-	float a,b,c, D;
+void vvod(float koef1, float koef2, float koef3, float n_koef);
+void vivod(float Disc, float kor1, float kor2);
+void resh(float koef1, float koef2, float koef3, float Disc);
+int main()
+{
+    float n_koef;
 	printf("Введите коэффициенты квадратного уравнения. Для завершения программы введите q q q.\n");
-	while (scanf("%f %f %f", &a, &b, &c) == 3)
-	{
-        D=b*b-4*a*c;
-        if (D<0)
-            printf("Уравнение не имеет решений в действительных числах.\n");
-        else if (D==0)
-        {
-            printf("Уравнение имеет одно решение, равное %f.\n", (-b+sqrt(D))/(2*a));
-        }
-        else if (D>0)
-        {
-            printf("Уравнение имеет два решения: %f и %f.\n", (-b+sqrt(D))/(2*a), (-b-sqrt(D))/(2*a));
-        }
+	vvod();
+	while (n_koef == 3)
+    {
+        resh();
+        vivod();
+        vvod();
     }
     printf("Удачи!");
     return 0;
+}
+void vivod(float Disc, float kor1, float kor2)
+{
+    if Disc<0
+        printf("Уравнение не имеет решений в действительных числах.\n");
+    else if Disc==0
+        printf("Уравнение имеет одно решение, равное %f.\n", kor1);
+    else if Disc>0
+        printf("Уравнение имеет два решения: %f и %f.\n", kor1, kor2);
+}
+void resh(float koef1, float koef2, float koef3, float Disc)
+{
+    Disc=koef2*koef2-4*koef1*koef3;
+    if Disc>0
+    {
+        kor1=(-koef2+sqrt(Disc))/(2*koef1);
+        kor2=(-koef2-sqrt(Disc))/(2*koef1);
+    }
+    else if Disc==0
+        kor1=(-koef2)/(2*koef1);
+}
+void vvod(float koef1, float koef2, float koef3, float n_koef)
+{
+    n_koef = scanf("%f %f %f", &koef1, &koef2, &koef3);
 }
