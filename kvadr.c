9f7c0478e17be508df414e86160d69d4459beed2
andreyphm <andrewkrokhin@gmail.com>
2025-08-22 20:07:47 +0300

    работает только если кэфы через пробел

--- kvadr.c
+++ kvadr.c
@@ -2,67 +2,82 @@
 #include <TXLib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <assert.h>
 
-const double NUMBER_CLOSE_TO_ZERO = 0.000001;
-const int INF_NUM_OF_SOLUTIONS = -1;
+const double NUMBER_CLOSE_TO_ZERO = 10e-6;
+
+enum num_of_roots {
+    INF_NUM_OF_SOLUTIONS = -1,
+    NO_SOLUTIONS         = 0,
+    ONE_SOLUTION         = 1,
+    TWO_SOLUTIONS        = 2
+};
 
 void input_coefficients(double*, bool*);
-void clear_input_buffer_and_end_of_program_check(int*);
+void clear_input_buffer();
+
 void equation_solver(double*, double*, int*);
 void linear_equation_solver(double*, double*, int*);
 void quadratic_equation_solver(double*, double*, int*);
-int is_close_to_zero(double*);
+
+int  is_close_to_zero(double);
+
 void output_answer(double*, int);
 
 int main()
 {
     double coefficients[3] = {}, answers[2] = {};
-    int number_of_answers = 0;
+    int number_of_answers = NO_SOLUTIONS;
     bool quit_program = false;
 
-    while (1)
+    while (!quit_program)
     {
         input_coefficients(coefficients, &quit_program);
         if (quit_program == true)
-            break;
+            continue;
         equation_solver(coefficients, answers, &number_of_answers);
         output_answer(answers, number_of_answers);
     }
 
-    printf("Программа завершена.\n");
+    printf("                   .\n");
     return 0;
 }
 
 void input_coefficients(double *coefficients, bool *quit_program)
 {
-    int number_of_coefficients = 0, number_of_q = 0;
+    assert (coefficients);
+    assert (quit_program);
+
+    int number_of_coefficients = 0;
+    char input_row[200] = {};
+
     while (number_of_coefficients != 3)
     {
-        printf("Введите коэффициенты квадратного уравнения. Для завершения введите q q q.\n");
-        number_of_coefficients = scanf("%lf %lf %lf", &coefficients[0], &coefficients[1], &coefficients[2]);
-        clear_input_buffer_and_end_of_program_check(&number_of_q);
-        if (number_of_q == 3)
-        {
+        printf("                                          .\n");
+        number_of_coefficients = sscanf(input_row, "%lg %lg %lg", &coefficients[0], &coefficients[1], &coefficients[2]);
+        if (input_row[0] == '\n')
             *quit_program = true;
             break;
-        }
+        clear_input_buffer();
     }
 }
 
-void clear_input_buffer_and_end_of_program_check(int *number_of_q)
+void clear_input_buffer()
 {
     int entered_character = 0;
-    do
-    {
+    do {
         entered_character = getchar();
-        if (entered_character == 'q')
-            *number_of_q = *number_of_q + 1;
-    }   while (entered_character != '\n' && entered_character != EOF);
+    } while (entered_character != '\n' && entered_character != EOF);
 }
 
 void equation_solver(double *coefficients, double *answers, int *number_of_answers)
 {
-    if (is_close_to_zero(&coefficients[0]))
+    assert (coefficients);
+    assert (answers);
+    assert (number_of_answers);
+    assert (coefficients != answers);
+
+    if (is_close_to_zero(coefficients[0]))
         linear_equation_solver(coefficients, answers, number_of_answers);
     else
         quadratic_equation_solver(coefficients, answers, number_of_answers);
@@ -70,43 +85,52 @@ void equation_solver(double *coefficients, double *answers, int *number_of_answe
 
 void linear_equation_solver(double *coefficients, double *answers, int *number_of_answers)
 {
-    if (is_close_to_zero(&coefficients[1]))
+    assert (coefficients);
+    assert (answers);
+    assert (number_of_answers);
+    assert (coefficients != answers);
+
+    if (is_close_to_zero(coefficients[1]))
     {
-        if (is_close_to_zero(&coefficients[2]))
+        if (is_close_to_zero(coefficients[2]))
             *number_of_answers = INF_NUM_OF_SOLUTIONS;
         else
-            *number_of_answers = 0;
+            *number_of_answers = NO_SOLUTIONS;
     }
     else
     {
         answers[0] = -coefficients[2] / coefficients[1];
-        *number_of_answers = 1;
+        *number_of_answers = ONE_SOLUTION;
     }
 }
 
 void quadratic_equation_solver(double *coefficients, double *answers, int *number_of_answers)
 {
-    double discriminant = 0;
-    discriminant = coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2];
+    assert (coefficients);
+    assert (answers);
+    assert (number_of_answers);
+    assert (coefficients != answers);
+
+    double discriminant = coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2];
 
     if (discriminant > 0)
     {
         answers[0] = (-coefficients[1] + sqrt(discriminant)) / (2 * coefficients[0]);
         answers[1] = (-coefficients[1] - sqrt(discriminant)) / (2 * coefficients[0]);
-        *number_of_answers = 2;
+        *number_of_answers = TWO_SOLUTIONS;
     }
-    else if (is_close_to_zero(&discriminant))
+    else if (is_close_to_zero(discriminant))
     {
         answers[0] = -coefficients[1] / (2 * coefficients[0]);
-        *number_of_answers = 1;
+        *number_of_answers = ONE_SOLUTION;
     }
-    else if (discriminant < 0)
-        *number_of_answers = 0;
+    else
+        *number_of_answers = NO_SOLUTIONS;
 }
 
-int is_close_to_zero (double *number_being_checked)
+int is_close_to_zero (double number_being_checked)
 {
-    if (fabs((*number_being_checked) < NUMBER_CLOSE_TO_ZERO))
+    if (fabs(number_being_checked) < NUMBER_CLOSE_TO_ZERO)
         return 1;
     else
         return 0;
@@ -114,19 +138,21 @@ int is_close_to_zero (double *number_being_checked)
 
 void output_answer(double *answers, int number_of_answers)
 {
+    assert (*answers);
+
     switch (number_of_answers)
     {
-        case 0:
-            printf("Уравнение не имеет решений.\n");
+        case NO_SOLUTIONS:
+            printf("                          .\n");
             break;
-        case 1:
-            printf("Уравнение имеет одно решение, равное %f.\n", answers[0]);
+        case ONE_SOLUTION:
+            printf("                            ,        %g.\n", answers[0]);
             break;
-        case 2:
-            printf("Уравнение имеет два решения: %f и %f.\n", answers[0], answers[1]);
+        case TWO_SOLUTIONS:
+            printf("                           : %g   %g.\n", answers[0], answers[1]);
             break;
         case INF_NUM_OF_SOLUTIONS:
-            printf("Корень уравнения - любое число.\n");
+            printf("                 -            .\n");
         default:
             break;
     }
